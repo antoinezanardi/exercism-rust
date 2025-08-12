a05d8777a6cebc2fcbf14c2f7a052e78847a8ca3
Antoine ZANARDI <antoine.zanardi.partner@decathlon.com>
2025-08-12 12:09:30 +0200

    feat: refactor anagram function to improve readability and reuse sorting logic

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,17 +1,22 @@
 use std::collections::HashSet;
 
+fn get_sorted_word(word: String) -> Vec<char> {
+    let mut word_characters: Vec<char> = word.chars().collect();
+    word_characters.sort_unstable();
+
+    word_characters
+}
+
 fn is_anagram(word: &str, candidate: &str) -> bool {
     let lowered_word = word.to_lowercase();
     let lowered_candidate = candidate.to_lowercase();
     if lowered_word == lowered_candidate || lowered_word.len() != lowered_candidate.len() {
         return false;
     }
-    let mut word_characters: Vec<char> = lowered_word.chars().collect();
-    let mut candidate_characters: Vec<char> = lowered_candidate.chars().collect();
-    word_characters.sort_unstable();
-    candidate_characters.sort_unstable();
+    let sorted_word = get_sorted_word(lowered_word);
+    let sorted_candidate = get_sorted_word(lowered_candidate);
 
-    word_characters == candidate_characters
+    sorted_word == sorted_candidate
 }
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
